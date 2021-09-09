fb97cd86f7f46c5ac70666063970a0ba8e92788d
gengen <gengen0630@gmail.com>
2021-09-09 19:19:41 +0900

    [add] save model

--- train.py
+++ train.py
@@ -11,6 +11,7 @@ from model import MelspClassifier
 
 
 def train_model(model, dataloaders_dict, criterion, optimizer, num_epochs):
+    best_val_acc = 0.0
     for epoch in range(1, num_epochs + 1):
         print(f'Epoch {epoch + 1}/{num_epochs}')
         print('-' * 20)
@@ -31,17 +32,28 @@ def train_model(model, dataloaders_dict, criterion, optimizer, num_epochs):
                     loss = criterion(outputs, labels)
                     _, preds = torch.max(outputs, 1)
 
+                    # 訓練時のみ逆伝播して更新
                     if phase == 'train':
                         loss.backward()
                         optimizer.step()
 
                     epoch_loss += loss.item() * melsps.size(0)
                     epoch_corrects += torch.sum(preds == labels.data)
+                    break
 
             num_data = len(dataloaders_dict[phase].dataset)
             epoch_loss = epoch_loss / num_data
             epoch_acc = epoch_corrects.double() / num_data
             print(f'{phase} Loss: {epoch_loss:.4f} Accuracy: {epoch_acc:.4f}')
+
+            # validationデータでのaccuracyが上がればそのモデルを保存
+            if phase == 'val' and epoch_acc > best_val_acc:
+                best_val_acc = epoch_acc
+                print(str(best_val_acc.item()))
+                _best_val_acc = str(best_val_acc.item())[:5].replace('.', '')
+                save_path = f'models/ep{epoch}_val-acc{_best_val_acc}.pt'
+                torch.save(model.state_dict(), save_path)
+                print(f'[SAVE] epoch {epoch} model to {save_path}')
 
 
 if __name__ == '__main__':
